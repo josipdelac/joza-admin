9a63875696d01442a496f1b4eea65f0cb4571b18
Josip Poljak <poljak.17@gmail.com>
2023-08-25 23:47:36 +0200

    Dodan novi login

--- backend/app.py
+++ backend/app.py
@@ -1,38 +1,131 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import bcrypt
+import random
+import traceback
+from flask import Flask, request, jsonify, send_file
+from passlib.hash import sha256_crypt
 import mysql.connector
+from flask_cors import CORS, cross_origin
+import io
+import base64
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
 
-# Povezivanje s MySQL bazom podataka
+CORS(app, resources={r"/api/*": {"origins": "*", "methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"}})
+
+# Konfiguracija baze podataka
 db = mysql.connector.connect(
     host="localhost",
-    user="root",  # Vaš korisničko ime
-    password="",  # Vaša lozinka
-    database="smartdoc"
+    user="root",
+    password="",
+    database="uiapp"
 )
 
-cursor = db.cursor()
+# Funkcija za generiranje "soli" na temelju slova hrvatske abecede
+def generate_salt():
+    hrvatska_abeceda = 'abcčćdđefghijklmnopqrsštuvwxyzž'
+    return random.choice(hrvatska_abeceda)
 
+
+# Funkcija za generiranje "papra" na temelju ime, prezime i emaila
+def generate_pepper(first_name, last_name, email):
+    email_prefix = email.split('@')[1]
+    return f"{first_name.lower()}{last_name.lower()}{email_prefix}"
+
+#Api za registraciju
+@app.route('/api/register', methods=['POST'])
+@cross_origin(origins='http://localhost:3000', methods=['POST'])
+
+
+def register():
+    data = request.get_json()
+    print("Received data:", data)  
+    try:
+        first_name = data['first_name']
+        last_name = data['last_name']
+        email = data['email']
+        country = data['country']
+        password = data['password']
+        if 'profile_image' in data:
+            profile_image_base64 = data['profile_image']     # Dobivanje slike iz JSON-a
+            # Pretvorba base64 stringa u binarni sadržaj
+            profile_image_data = base64.b64decode(profile_image_base64)
+        else:
+            profile_image_data:None
+
+
+        # Generiranje soli i papra
+        salt = generate_salt()
+        pepper = generate_pepper(first_name, last_name, email)
+        print (salt)
+        print (pepper)
+
+        
+
+        # Kombiniranje soli, papra i lozinke te hasiranje
+        combined_password = password + salt + pepper
+        
+        hashed_password = sha256_crypt.hash(combined_password)
+        print ("Password je "+password+" Sol je "+salt+" Biber je "+pepper+" hesirana je: "+hashed_password)
+
+        # Pohranjivanje u bazu
+        
+        cursor = db.cursor()
+        query = "INSERT INTO users (first_name, last_name, email, country, password, profile_image) VALUES (%s, %s, %s, %s, %s, %s)"
+        cursor.execute(query, (first_name, last_name, email, country, hashed_password, profile_image_data))
+        db.commit()
+        cursor.close()
+        
+
+       
+        return jsonify({'message': 'User registered successfully'})
+    except Exception as e:
+        print("Error:", e) 
+        traceback.print_exc()  # Ispis detalja greške
+
+        return jsonify({'message': 'Error registering user'}), 500
+
+#Api za login
 @app.route('/api/login', methods=['POST'])
+@cross_origin(origins='http://localhost:3000', methods=['POST'])
+
 def login():
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
-    print("Dogodilo se nes", str(request))
-    query = "SELECT username, password FROM users WHERE username = %s"
-    cursor.execute(query, (username,))
-    user = cursor.fetchone()
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
 
-    if not user:
-        return jsonify({"message": "Invalid credentials"}), 401
+    # Dohvaćanje korisnika iz baze prema emailu
+    cursor = db.cursor()
+    query = "SELECT id, password, first_name, last_name FROM users WHERE email = %s"
+    cursor.execute(query, (email,))
+    user_data = cursor.fetchone()
+    
 
-    if bcrypt.checkpw(password.encode('utf-8'), user[1].encode('utf-8')):
-        return jsonify({"message": "Login successful"})
+    if user_data:
+        stored_password = user_data[1]
+        first_name = user_data[2]
+        last_name = user_data[3]
+        pepper = generate_pepper(first_name, last_name, email)  # Generiraj "papar" kao kod registracije
+
+        # Pokušaj različite soli dok se ne pronađe točna kombinacija lozinke
+        for char in 'abcčćdđefghijklmnopqrsštuvwxyzž':
+            salt = char
+            combined_password = password + salt + pepper
+            print(pepper)
+            hashed_combined_password = sha256_crypt.hash(combined_password)
+            print ("Password je "+password+" Sol je "+salt+" Biber je "+pepper+" hesirana je: "+hashed_combined_password)
+            
+            print(hashed_combined_password+"----"+stored_password)
+            if sha256_crypt.verify(combined_password, stored_password):
+                print("pogodak")
+                return jsonify({'message': 'Login successful'})
+                break
+            
     else:
-        return jsonify({"message": "Invalid credentials"}), 401
+        return jsonify({'message': 'User not found'}), 404
+    
+    cursor.close()
+
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
