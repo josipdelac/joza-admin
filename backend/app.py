71bbd9bb0280fc965edb04645512afe85b34f361
Josip Poljak <poljak.17@gmail.com>
2023-08-26 19:49:11 +0200

    rade drzave

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,10 @@ import mysql.connector
 from flask_cors import CORS, cross_origin
 import io
 import base64
+import xml.etree.ElementTree as ET
+import requests
+import concurrent
+
 
 app = Flask(__name__)
 
@@ -19,6 +23,9 @@ db = mysql.connector.connect(
     database="uiapp"
 )
 
+
+
+
 # Funkcija za generiranje "soli" na temelju slova hrvatske abecede
 def generate_salt():
     hrvatska_abeceda = 'abcčćdđefghijklmnopqrsštuvwxyzž'
@@ -29,6 +36,32 @@ def generate_salt():
 def generate_pepper(first_name, last_name, email):
     email_prefix = email.split('@')[1]
     return f"{first_name.lower()}{last_name.lower()}{email_prefix}"
+
+@app.route('/api/countries', methods=['POST'])
+def proxy_countries_request():
+    try:
+        # Dohvatite SOAP zahtjev iz tijela zahtjeva
+        soap_request = request.data
+
+        # Šaljite SOAP zahtjev prema vanjskoj web usluzi
+        response = requests.post('http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso', data=soap_request, headers={'Content-Type': 'application/soap+xml'})
+
+        # Parsiranje XML odgovora
+        root = ET.fromstring(response.content)
+        country_elements = root.findall(".//{http://www.oorsprong.org/websamples.countryinfo}tCountryCodeAndName")
+
+        # Izdvajanje imena država
+        country_names = [country.find("{http://www.oorsprong.org/websamples.countryinfo}sName").text for country in country_elements]
+
+        # Vratite imena država kao JSON
+        print (country_names)
+        return jsonify(country_names)
+
+    except Exception as e:
+        print("Error:", e)
+        return jsonify([])
+    
+
 
 #Api za registraciju
 @app.route('/api/register', methods=['POST'])
@@ -43,6 +76,9 @@ def register():
         last_name = data['last_name']
         email = data['email']
         country = data['country']
+
+        
+
         password = data['password']
         if 'profile_image' in data:
             profile_image_base64 = data['profile_image']     # Dobivanje slike iz JSON-a
@@ -50,6 +86,8 @@ def register():
             profile_image_data = base64.b64decode(profile_image_base64)
         else:
             profile_image_data:None
+
+        
 
 
         # Generiranje soli i papra
@@ -104,6 +142,11 @@ def login():
         first_name = user_data[2]
         last_name = user_data[3]
         pepper = generate_pepper(first_name, last_name, email)  # Generiraj "papar" kao kod registracije
+        ##Threads
+        # with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        #     for index in range(8):
+        #         for char in 'abcčćdđefghijklmnopqrsštuvwxyzž':
+        #             executor.submit(find_match(char,password,pepper,stored_password), )
 
         # Pokušaj različite soli dok se ne pronađe točna kombinacija lozinke
         for char in 'abcčćdđefghijklmnopqrsštuvwxyzž':
@@ -117,14 +160,24 @@ def login():
             if sha256_crypt.verify(combined_password, stored_password):
                 print("pogodak")
                 return jsonify({'message': 'Login successful'})
-                break
+                
             
     else:
         return jsonify({'message': 'User not found'}), 404
     
     cursor.close()
 
-
+def find_match(salt,password,pepper, stored_password):
+    salt = char
+    combined_password = password + salt + pepper
+    print(pepper)
+    hashed_combined_password = sha256_crypt.hash(combined_password)
+    print ("Password je "+password+" Sol je "+salt+" Biber je "+pepper+" hesirana je: "+hashed_combined_password)
+    
+    print(hashed_combined_password+"----"+stored_password)
+    if sha256_crypt.verify(combined_password, stored_password):
+        print("pogodak")
+        return jsonify({'message': 'Login successful'})
     
 
 if __name__ == '__main__':
